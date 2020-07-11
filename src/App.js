83d18a5e9e072a0cd18444be648c559165176adc
Scott MacDonald <scottmacd88@yahoo.com>
2020-07-11 15:07:28 -0700

    Added API and ajax call with MDBtables.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css';
 import Employees from "./components/Employees";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+
+
+
 
 function App() {
   return (
--- src/components/Employees.js
+++ src/components/Employees.js
@@ -1,23 +1,73 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { MDBDataTable } from "mdbreact";
 
 class Employees extends Component {
 
     state = {
-        employeeData: []
+        employeeRecords: []
     };
 
     componentDidMount = () => {
         axios.get("https://randomuser.me/api/?results=200&nat=us")
-        .then(response => {
-            console.log(response);
-        });
+            .then(response => {
+                console.log(response);
+                var employeeRecords = []
+                for (let i = 0; i < response.data.results.length; i++) {
+                    let employee = {
+                        firstName: response.data.results[i].name.first,
+                        lastName: response.data.results[i].name.last,
+                        email: response.data.results[i].email,
+                        phoneNum: response.data.results[i].cell
+                    }
+                    employeeRecords.push(employee)
+                    console.log(employeeRecords);
+                }
+                this.setState({ employeeRecords: employeeRecords });
+                console.log(employeeRecords,this.state.employeeRecords);
+            });
     };
 
-    render () {
-        return(
+    render() {
+
+        var employeeData = {
+            columns: [
+                {
+                    label: "First Name",
+                    field: "firstName",
+                    sort: "asc",
+                    width: 150
+
+                },
+                {
+                    label: "lastName",
+                    field: "lastName",
+                    sort: "asc",
+                    width: 150
+
+                },
+                {
+                    label: "email",
+                    field: "email",
+                    sort: "asc",
+                    width: 150
+
+                },
+                {
+                    label: "phoneNum",
+                    field: "phoneNum",
+                    sort: "asc",
+                    width: 150
+
+                }
+
+            ],
+            rows: this.state.employeeRecords
+        }
+
+        return (
             <div>
-                <h1>This is a Heading</h1>
+                <MDBDataTable bordered responsive striped data={employeeData}/>
             </div>
         )
     }
