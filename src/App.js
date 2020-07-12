7b408b92c60328d7f2f9c8179759bd8d6fe667b3
Scott MacDonald <scottmacd88@yahoo.com>
2020-07-12 08:35:01 -0700

    Functionality present. Working on CSS next

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import Employees from "./components/Employees";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
-
+import Header from "./components/Header";
 
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Testing</h1>
+      <Header />
       <Employees />
       
     </div>
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div>
+            <nav className="navbar navbar-dark bg-dark">
+                <h1 className="text-white text-center">BigMac Web Design</h1>
+            </nav>
+        </div>
+    )
+}
+
+export default Header;
