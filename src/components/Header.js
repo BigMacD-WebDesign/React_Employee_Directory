98059e2063de212cac0408719561a5c9a51575fd
Scott MacDonald <scottmacd88@yahoo.com>
2020-07-13 18:44:22 -0700

    Cleaning code. Modifying Bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,7 @@ import React from "react";
 
 function Header() {
     return (
-        // <div>
-        //     <nav className="navbar navbar-primary bg-primary text-light">
-        //         <h1>BigMac Web Design, Inc</h1>
-        //     </nav>
-        // </div>
+        //Header Section displaying Jumbotron from Bootstrap.
         <div className="jumbotron jumbotron-fluid bg-primary">
             <div className="container">
                 <h1 className="display-2 col-lg-12 col-sm-12 text-white">BigMac Web Design, Inc</h1>
