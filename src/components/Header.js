c7244f57e3c7473b09a930e4a397f70730c7eb35
Scott MacDonald <scottmacd88@yahoo.com>
2020-07-13 18:08:51 -0700

    Changed the Bootstrap from Nav to Jumbotron. Responsive.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,15 @@ import React from "react";
 
 function Header() {
     return (
-        <div>
-            <nav className="navbar navbar-primary bg-primary text-light">
-                <h1>BigMac Web Design, Inc</h1>
-            </nav>
+        // <div>
+        //     <nav className="navbar navbar-primary bg-primary text-light">
+        //         <h1>BigMac Web Design, Inc</h1>
+        //     </nav>
+        // </div>
+        <div className="jumbotron jumbotron-fluid bg-primary">
+            <div className="container">
+                <h1 className="display-2 col-lg-12 col-sm-12 text-white">BigMac Web Design, Inc</h1>
+            </div>
         </div>
     )
 }
